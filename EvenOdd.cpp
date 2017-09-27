9375da4e5468ae2cfc3199b94d82c51fdb15e729
Audie Warren <32240482+AudieWarren@users.noreply.github.com>
2017-09-27 10:46:02 +0100

    Add files via upload

---
+++ EvenOdd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int x;
+while (1==1){
+    cin >> x;
+    if (x % 2 == 0) {
+        cout << "Even" << endl;
+    }
+    else {
+        cout << "Odd" << endl;
+    }}
+    return 0;
+}
+
